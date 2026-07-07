a0f5c4cedcea8f7cd5ea64d90df52147c92cfeaf
tadelakiran <brochaitanya55@gmail.com>
2026-07-07 14:15:00 +0530

    backend added

--- src/apis/AuthApis.ts
+++ src/apis/AuthApis.ts
@@ -2,5 +2,5 @@ import type { RegisterRequest } from "../interfaces/RegitserRequest";
 import axiosInstance from "./AxiosConfig";
 
 export const registerUser = async(data: RegisterRequest) => {
-    return await axiosInstance.post("/auth/register", data);
+    return await axiosInstance.post("/customers", data);
 };
